2d05b5fb432304ecc2c6859e0e63c263db378b23
erkinov97 <erkinov997@gmail.com>
2024-04-07 19:09:04 +0500

    feat: create card and not found components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { CardComponent } from './card/card.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CardComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
